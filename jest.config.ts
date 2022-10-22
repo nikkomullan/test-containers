1e4106129b5b5c4f06b6bb930f442bc18705d0e4
Nicolas Martinez <nicolas.martinez@volvocars.com>
2022-10-22 21:36:21 +0200

    Update jest setup

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest'
+
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  projects: [
+    {
+      displayName: 'test',
+    },
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/src/**/*.spect.ts'],
+      preset: 'ts-jest',
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/integration/**/*.spec.ts'],
+      preset: 'ts-jest',
+    },
+  ],
+} as Config
